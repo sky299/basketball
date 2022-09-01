924984772e3365a4dc98db40d0b57ef6a39fcc4d
WsDaren <2573992307@qq.com>
2022-09-01 19:59:48 +0800

    新增图片，视频显示

--- src/main/java/com/jckj/controller/MienController.java
+++ src/main/java/com/jckj/controller/MienController.java
@@ -2,6 +2,7 @@ package com.jckj.controller;
 
 import com.jckj.model.Mien;
 import com.jckj.service.MienService;
+import com.jckj.util.FileUploadUtil;
 import com.jckj.vo.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * @author: SkLily
@@ -28,22 +30,24 @@ public class MienController {
         return JsonResult.success(mienService.list(mien).getList(),mienService.count(mien));
     }
     @RequestMapping("insert")
-    public Integer insert(Mien mien, MultipartFile img,MultipartFile video) throws IOException {
+    public Integer insert(Mien mien, MultipartFile img,MultipartFile video) throws Exception {
         if (img != null) {
-            // 设置图片路径
+            // 获取图片名
             String filename = img.getOriginalFilename();
-            String filepath = "E://Picture//g//"+filename;
-            // 转存图片
-            img.transferTo(new File(filepath));
-            mien.setMienPhoto(filename);
+            //防止重名
+            String imgname = UUID.randomUUID().toString()+filename;
+            //上传到七牛云
+            FileUploadUtil.fileUploadToQiNiu(img.getBytes(), imgname);
+            mien.setMienPhoto("http://rhiqmuv5p.hn-bkt.clouddn.com/" + imgname);
         }
         if (video != null) {
             // 设置视频路径
             String filename = video.getOriginalFilename();
-            String filepath = "E://Picture//g//"+filename;
-            // 转存视频
-            video.transferTo(new File(filepath));
-            mien.setMienVideo(filename);
+            //防止重名
+            String videoname = UUID.randomUUID().toString()+filename;
+            //上传到七牛云
+            FileUploadUtil.fileUploadToQiNiu(video.getBytes(), videoname);
+            mien.setMienVideo("http://rhiqmuv5p.hn-bkt.clouddn.com/" + videoname);
         }
         Integer insert = mienService.insert(mien);
         return insert;
