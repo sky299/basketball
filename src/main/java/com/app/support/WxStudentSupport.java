c264c1852b59a77768eb288bb584e12c3f7028d2
sky295 <463731963@qq.com>
2022-09-08 12:49:59 +0800

    小程序学员列表和学员详情

--- src/main/java/com/app/support/WxStudentSupport.java
+++ src/main/java/com/app/support/WxStudentSupport.java
@@ -1,7 +1,7 @@
 package com.app.support;
 
-import com.app.service.WxClassStudentService;
-import com.app.service.WxStudentService;
+import com.app.mapper.WxClassStudentMapper;
+import com.app.mapper.WxStudentMapper;
 import com.jckj.model.TRClassStudent;
 import com.jckj.model.TStudentInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,19 +11,20 @@ import org.springframework.stereotype.Component;
 public class WxStudentSupport {
 
     @Autowired
-    private WxStudentService wxStudentService;
+    private WxStudentMapper wxStudentMapper;
 
     @Autowired
-    private WxClassStudentService wxClassStudentService;
+    private WxClassStudentMapper wxClassStudentMapper;
 
     public String getStudentName(Integer studentId) {
-        TStudentInfo tStudentInfo = wxStudentService.info(studentId);
+        TStudentInfo tStudentInfo = wxStudentMapper.info(studentId);
         return tStudentInfo.getStudentName();
     }
 
     public void addStudentCourseNum(Integer studentId) {
-        TRClassStudent trClassStudent = wxClassStudentService.infoByStudentId(studentId);
+        TRClassStudent trClassStudent = wxClassStudentMapper.infoByStudentId(studentId);
         trClassStudent.setStudentCourseNum(trClassStudent.getStudentCourseNum() + 1);
-        wxClassStudentService.update(trClassStudent);
+        trClassStudent.setUpdateTime(System.currentTimeMillis());
+        wxClassStudentMapper.update(trClassStudent);
     }
 }
